ab14cbdba8e1a74603bb03ef6af1da4e22ea96b7
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-02-12 11:36:08 -0500

    Load homepage when header name is clicked

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { loadMenu } from "./menu.js";
 import { loadAbout } from "./about.js";
 import { loadContact } from "./contact.js";
 
+const header = document.querySelector("header");
+const headerName = header.querySelector("p");
 const homeBtn = document.querySelector(".home-btn");
 const menuBtn = document.querySelector(".menu-btn");
 const aboutBtn = document.querySelector(".about-btn");
@@ -11,6 +13,7 @@ const contactBtn = document.querySelector(".contact-btn");
 
 loadHome();
 
+headerName.addEventListener("click", loadHome);
 homeBtn.addEventListener("click", loadHome);
 menuBtn.addEventListener("click", loadMenu);
 aboutBtn.addEventListener("click", loadAbout);
